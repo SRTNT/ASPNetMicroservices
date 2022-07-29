4a49162605bbb0a2e26419a03b3ecc42b7721285
Saeed Reza <s.r.taheri@gmail.com>
2022-07-29 23:10:46 +0430

    W2

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Register REDIS Location
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CachSettings:ConnectionSetting");
+});
+
+services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
