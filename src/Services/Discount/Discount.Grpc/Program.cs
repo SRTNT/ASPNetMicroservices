89f47e23fec3ef48d568eacd68c8d2152e147fbb
Saeed Reza <s.r.taheri@gmail.com>
2022-08-10 21:07:55 +0430

    Finished Discount GRPC

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,7 @@
+using Discount.Grpc;
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
+using Discount.Grpc.Repositories.Interfaces;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +10,25 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddScoped<Discount.Grpc.Repositories.Interfaces.IDiscountRepository, Discount.Grpc.Repositories.DiscountRepository>();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(SRT));
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+app.MigrateDatabase();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+namespace Discount.Grpc
+{
+    public class SRT
+    {
+
+    }
+}
